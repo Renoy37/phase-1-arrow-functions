81d08f67ea1dc18507a3f6232e8bdabccbaa7090
Robins <robinmgk077@gmail.com>
2023-12-14 11:27:59 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,18 @@
+const divide = function (num1, num2) {
+  if (num1 !== undefined && num2 !== undefined) {
+    return num1 / num2;
+  } else {
+    return 2000 / 100;
+  }
+};
+// const divide = function (num1, num2) {
+//   return num1 / num2;
+// };
+divide(2000,100)
+
+
+const square = (x) => x * x;
+square(2);
+
+const add = (param1, param2) => param1 + param2;
+add(3, 4);
